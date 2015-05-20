015bd72167b40b280ffc715567ba6c3eb5771897
Hans-Christian Andersen <hans@powershifter.com>
2015-05-20 15:55:49 -0700

    Updated deploy config

--- deploy.php
+++ deploy.php
@@ -1,14 +1,14 @@
+<?php
+date_default_timezone_set('UTC');
 require 'recipe/common.php';
-//require 'recipe/symfony.php';
+require 'recipe/composer.php';
+require 'config/deploy.php';
 
-// Define server for deploy.
-// Let's name it "main" and use 22 port.
-server('main', 'full.im.phulse.com', 22)
-    ->path('/var/www/full.im.phulse.com') // Define base path to deploy you project.
-    ->user('name', 'password');  // Define SSH user name and password.
-                                 // You can skip password and it will be asked on deploy.
-                                 // Also you can connect to server SSH via public keys and ssh config file.
+$config = new Deploy_Config();
 
-// Specify repository from which to download your projects code.
-// Server has to be able clone your project from this repository.
+server( 'droplet1', 'full.im.phulse.com', 22 )
+    ->user( $config->user )
+    ->env( 'deploy_path', $config->deploy_path )
+    ->identityFile( $config->id_rsa_pub, $config->id_rsa );
+
 set('repository', 'git@github.com:thebuccaneersden/full.im.phulse.com.git');
